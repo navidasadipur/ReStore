b55ca823b2e1f1b22736ea266c3241a01227bef0
navid.asadipur <navid.asadipur@gmail.com>
2023-02-16 14:05:58 +0330

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,31 +1,42 @@
 import { Container, createTheme, CssBaseline } from "@mui/material";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' : 'light';
+  const paletteType = darkMode ? "dark" : "light";
   const theme = createTheme({
     palette: {
       mode: paletteType,
       background: {
-        default: paletteType === 'light' ? '#eaeaea' : '#121212'
-      }
-    }
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
   });
 
-  function handleThemeChange(){
-    setDarkMode(prev => !prev )
+  function handleThemeChange() {
+    setDarkMode((prev) => !prev);
   }
 
   return (
-    <ThemeProvider theme={theme} >
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header isDarkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <Header isDarkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog/>
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:id" element={<ProductDetails />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          </Routes>
       </Container>
     </ThemeProvider>
   );
