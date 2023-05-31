695d6498b1fc43d91f01027cf97beb5e5ac630bf
n.asadipour <n.asadipour@charisma.ir>
2023-05-31 18:07:05 +0330

    add CreateProduct api. end of section 13.4

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,8 +1,11 @@
 using System.Text.Json;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Extensions;
 using API.RequestHelpers;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,8 +14,10 @@ namespace API.Controllers
     public class ProductsController : BaseApiController
     {
         private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IMapper _mapper;
+        public ProductsController(StoreContext context, IMapper mapper)
         {
+            _mapper = mapper;
             _context = context;
             
         }
@@ -34,7 +39,7 @@ namespace API.Controllers
             return products;
         } 
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name="GetProduct")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
@@ -51,6 +56,21 @@ namespace API.Controllers
             var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
 
             return Ok(new {brands, types});
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(CreateProductDto productDto)
+        {
+            var product = _mapper.Map<Product>(productDto);
+
+            _context.Products.Add(product);
+
+            var result = await _context.SaveChangesAsync() > 0;
+
+            if (result) return CreatedAtRoute("GetProduct", new {Id = product.Id}, product);
+
+            return BadRequest(new ProblemDetails {Title = "Problem creating new product"});
         }
     }
 }
