cbf3e6fe294bf0980df1d68cc6811bc2b508984c
n.asadipour <n.asadipour@charisma.ir>
2023-06-11 11:18:47 +0330

    add delete functionality to inventory page. end of section 13.19

--- client/src/features/admin/Inventory.tsx
+++ client/src/features/admin/Inventory.tsx
@@ -5,20 +5,32 @@ import AppPagination from "../../app/components/AppPagination";
 import { currencyFormat } from "../../app/util/util";
 import useProducts from "../../app/hooks/useProducts";
 import { useAppDispatch } from "../../app/store/configureStore";
-import { setPageNumber } from "../catalog/catalogSlice";
+import { removeProduct, setPageNumber } from "../catalog/catalogSlice";
 import { useState } from "react";
 import ProductForm from "./ProductForm";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
 
 export default function Inventory() {
     const {products, metaData} = useProducts();
     const dispatch = useAppDispatch();
     const [editMode, setEditMode] = useState(false);
     const [selectedProduct, setSelectedProduct] = useState<Product | undefined>(undefined);
+    const [loading, setLoading] = useState(false);
+    const [target, setTarget] = useState(0);
 
     function handleSelectProduct(product: Product) {
         setSelectedProduct(product);
         setEditMode(true);
+    }
+
+    function handleDeleteProduct(id: number) {
+        setLoading(true);
+        setTarget(id);
+        agent.Admin.deleteProduct(id)
+            .then(() => dispatch(removeProduct(id)))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false));
     }
 
     function cancleEdit() {
@@ -67,12 +79,11 @@ export default function Inventory() {
                                 <TableCell align="center">{product.brand}</TableCell>
                                 <TableCell align="center">{product.quantityInStock}</TableCell>
                                 <TableCell align="right">
-                                    { <Button onClick={() => handleSelectProduct(product)} startIcon={<Edit />} />
-                                    // <LoadingButton 
-                                    //     loading={loading && target === product.id} 
-                                    //     onClick={() => handleDeleteProduct(product.id)} 
-                                    //     startIcon={<Delete />} color='error' /> 
-                                }
+                                     <Button onClick={() => handleSelectProduct(product)} startIcon={<Edit />} />
+                                     <LoadingButton 
+                                         loading={loading && target === product.id} 
+                                         onClick={() => handleDeleteProduct(product.id)} 
+                                         startIcon={<Delete />} color='error' /> 
                                 </TableCell>
                             </TableRow>
                         ))}
